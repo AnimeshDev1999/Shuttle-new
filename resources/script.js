57c1025178f26a46e0a3086172387b27365d52ba
Animesh Kumar <animesh99dev@gmail.com>
2021-11-10 20:12:05 +0530

    completed the desktop version

--- resources/script.js
+++ resources/script.js
@@ -84,3 +84,48 @@ function dayNum() {
 }
 
 dayNum();
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".secondary-nav");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-60px",
+  }
+);
+obs.observe(sectionHeroEl);
